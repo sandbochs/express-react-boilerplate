161a7db60cbd44525ae4236e5996c0a4800eb735
Elliot Shiu <elliot@sandbochs.com>
2018-05-02 01:12:08 -0700

    add example spec

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // @flow
-export const port = process.env.port || 3000 // eslint-disable-line
+export const port = process.env.PORT || 3000 // eslint-disable-line
---
+++ test/http/v1/example.spec.js
@@ -0,0 +1,16 @@
+import request from 'supertest'
+
+import app from 'server'
+
+describe('/v1/example', () => {
+  describe('GET', () => {
+    test('should respond with the correct json', async () => {
+      request(app)
+        .get('/v1/example')
+        .expect(200)
+        .then((res) => {
+          expect(res.body).toEqual({ hello: 'world' })
+        })
+    })
+  })
+})
